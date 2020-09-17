9006d5042ea6bda9c7a654c1d9f6554bbb76dbc6
metacomp <jcf2_2003@yahoo.com>
2020-09-17 16:56:23 -0400

    Update run.py

--- sample_time_app/run.py
+++ sample_time_app/run.py
@@ -1,1 +1,12 @@
+from flask import Flask
+app = Flask(__name__)
 
+
+@app.route('/')
+def hello_world():
+    return 'Hello world!'
+
+
+app.run(host='0.0.0.0',
+        port=8080,
+        debug=True)
